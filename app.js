f1d6684b17490b37c04acc70ffe75ce746525fea
Vova Pol <ya-chai777@yandex.ru>
2023-01-10 12:31:23 +0300

    feat: написал мидлвэр для авторизации запросов

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const usersRouter = require('./routers/users');
 const cardsRouter = require('./routers/cards');
 const { postUser, login } = require('./controllers/users');
+const auth = require('./middlewares/auth');
 
 const { PORT = 3000 } = process.env;
 const DB_URL = 'mongodb://localhost:27017/mestodb';
@@ -21,10 +22,13 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use('/', usersRouter);
-app.use('/', cardsRouter);
 app.post('/signin', login);
 app.post('/signup', postUser);
+
+// app.use(auth);
+app.use('/', usersRouter);
+app.use('/', cardsRouter);
+
 app.use('*', (req, res) => {
   res.status(404).send({ message: 'Такой страницы не существует' });
 });
---
+++ middlewares/auth.js
@@ -0,0 +1,29 @@
+const jwt = require('jsonwebtoken');
+
+const unauthorizedErrCode = 401;
+
+module.exports = (req, res, next) => {
+  const { authorization } = req.headers;
+
+  if (!authorization || !authorization.startsWith('Bearer ')) {
+    console.log('auth: ' + authorization);
+    return res
+      .status(unauthorizedErrCode)
+      .send({ message: 'Необходима авторизация' });
+  }
+
+  const token = authorization.replace('Bearer ', '');
+  let payload;
+
+  try {
+    payload = jwt.verify(token, 'secret-key');
+  } catch (err) {
+    return res
+      .status(unauthorizedErrCode)
+      .send({ message: 'Необходима авторизация' });
+  }
+
+  req.user = payload;
+
+  next();
+};
