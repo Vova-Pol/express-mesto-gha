c493095b9934cf8dab1d8bf35e72d70bb5b0abd4
Vova Pol <ya-chai777@yandex.ru>
2023-01-21 18:26:46 +0300

    refactor: поправил функцию авторизации пользователя

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -6,6 +6,7 @@ module.exports = (req, res, next) => {
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
     next(new UnauthorizedErr('Необходима авторизация'));
+    return;
   }
 
   const token = authorization.replace('Bearer ', '');
