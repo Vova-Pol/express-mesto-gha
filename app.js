f8f9b3b159c0cf2d9412b1bec06a3951d816c083
Vova Pol <ya-chai777@yandex.ru>
2023-01-24 14:13:35 +0300

    refactor: перенес роуты регистрации, авторизации и всех ост.страниц в корневой роутер

--- app.js
+++ app.js
@@ -1,11 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const { errors, celebrate } = require('celebrate');
-const { postUser, login } = require('./controllers/users');
+const { errors } = require('celebrate');
 const auth = require('./middlewares/auth');
 const centralErrorHandling = require('./middlewares/centralErrorHandling');
-const NotFoundErr = require('./errors/not-found-error');
-const { signInConfig, signUpConfig } = require('./utils/celebrateValidConfig');
 const mainRouter = require('./routers');
 
 const { PORT = 3000 } = process.env;
@@ -14,15 +11,8 @@ const DB_URL = 'mongodb://localhost:27017/mestodb';
 const app = express();
 app.use(express.json());
 
-app.post('/signup', celebrate(signUpConfig), postUser);
-app.post('/signin', celebrate(signInConfig), login);
-
 app.use(auth);
 app.use('/', mainRouter);
-
-app.use('*', (req, res, next) => {
-  next(new NotFoundErr('Такой страницы не существует'));
-});
 
 // --- Обработка ошибок
 
--- routers/index.js
+++ routers/index.js
@@ -1,8 +1,19 @@
 const mainRouter = require('express').Router();
+const { celebrate } = require('celebrate');
 const cardsRouter = require('./cards');
 const usersRouter = require('./users');
+const { signUpConfig, signInConfig } = require('../utils/celebrateValidConfig');
+const { postUser, login } = require('../controllers/users');
+const NotFoundErr = require('../errors/not-found-error');
+
+mainRouter.post('/signup', celebrate(signUpConfig), postUser);
+mainRouter.post('/signin', celebrate(signInConfig), login);
 
 mainRouter.use('/cards', cardsRouter);
 mainRouter.use('/users', usersRouter);
+
+mainRouter.use('*', (req, res, next) => {
+  next(new NotFoundErr('Такой страницы не существует'));
+});
 
 module.exports = mainRouter;
