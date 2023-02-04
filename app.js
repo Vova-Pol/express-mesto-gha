577588c2abfd78f6037afc40f566e1a0ce344b08
Vova Pol <ya-chai777@yandex.ru>
2023-02-04 17:38:16 +0300

    feat: реализовал логгирование запросов и ошибок

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const { errors } = require('celebrate');
 const centralErrorHandling = require('./middlewares/centralErrorHandling');
 const mainRouter = require('./routers');
+const { requestLogger, errorLogger } = require('./middlewares/logger');
 
 const { PORT = 3000 } = process.env;
 const DB_URL = 'mongodb://localhost:27017/mestodb';
@@ -10,9 +11,13 @@ const DB_URL = 'mongodb://localhost:27017/mestodb';
 const app = express();
 app.use(express.json());
 
+app.use(requestLogger);
+
 app.use('/', mainRouter);
 
 // --- Обработка ошибок
+
+app.use(errorLogger);
 
 app.use(errors());
 app.use(centralErrorHandling);
